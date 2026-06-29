9cadc10bb0d53f428c8496e249457c5bce82b617
Austin Robertson <162934591+arobertson7@users.noreply.github.com>
2026-06-28 22:53:19 -0400

    all pages created in src/pages and routes added in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CampusesPage from "./pages/CampusesPage";
+import CampusDetailPage from "./pages/CampusDetailPage";
+import StudentsPage from "./pages/StudentsPage";
+import StudentDetailPage from "./pages/StudentDetailPage";
+import AddCampusPage from "./pages/AddCampusPage";
+import AddStudentPage from "./pages/AddStudentPage";
+import EditCampusPage from "./pages/EditCampusPage";
+import EditStudentPage from "./pages/EditStudentPage";
+import NotFoundPage from "./pages/404Page";
 
-function App() {
+export default function App() {
 
   return (
     <>
       <h1 className="text-4xl font-bold text-blue-600">hello</h1>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="campuses" element={<CampusesPage />} />
+        <Route path="campuses/:id" element={<CampusDetailPage />} />
+        <Route path="students" element={<StudentsPage />} />
+        <Route path="students/:id" element={<StudentDetailPage />} />
+        <Route path="campuses/add" element={<AddCampusPage />} />
+        <Route path="students/add" element={<AddStudentPage />} />
+        <Route path="campuses/edit/:id" element={<EditCampusPage />} />
+        <Route path="students/edit/:id" element={<EditStudentPage />} />
+        <Route path ="*" element ={<NotFoundPage />} />
+      </Routes>
     </>
   )
   
 }
-
-export default App
